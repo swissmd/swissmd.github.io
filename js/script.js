1071ac28b7a58267195d8a58123b5eab4ac12900
SwissMD <md@millenniumppp.com>
2023-04-19 16:30:54 -0700

    js

--- js/script.js
+++ js/script.js
@@ -7,25 +7,25 @@ console.log("Testing my js link");
 // script for hero animated carousel via https://jsfiddle.net/Grobbert/gkder2Lw/2/
 // carousel will only work when page is live
 
-// $('.owl-carousel').owlCarousel({
-//     loop:true,
-//     margin:10,
-//     dots:false,
-//     nav:true,
-//     mouseDrag:false,
-//     autoplay:true,
-//     animateOut: 'slideOutUp',
-//     responsive:{
-//         0:{
-//             items:1
-//         },
-//         600:{
-//             items:1
-//         },
-//         1000:{
-//             items:1
-//         }
-//     }
-// });
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    dots:false,
+    nav:true,
+    mouseDrag:false,
+    autoplay:true,
+    animateOut: 'slideOutUp',
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+});
 
 
